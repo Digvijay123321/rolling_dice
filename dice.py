c8f50c8924419839b6b217de9de12525cbf452b5
Digvijay <52031713+Digvijay123321@users.noreply.github.com>
2020-02-24 18:41:55 +0530

    Create dice.py

---
+++ dice.py
@@ -0,0 +1,19 @@
+import random
+
+def roll(sides=6):
+    num_rolled = random.randint(1,sides)
+    return num_rolled
+def main():
+    sides = 6
+    rolling = True
+    while rolling:
+        roll_again = input('''Ready to roll?
+            \tEnter=Roll. Q=Quit. :''')
+        if roll_again.lower() == "q":
+            rolling=False
+        else:
+            num_rolled = roll(sides)
+            print("\t\tYou rolled : ",num_rolled)
+
+    print("\t \t Thanks for playing!!")
+main()
